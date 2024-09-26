fb1b002d693bc54d9faf8a9cdf01ffdb47b36efa
sonesiji <sonesiji2020@gmail.com>
2024-09-26 17:17:23 +0530

    My

--- college_app/urls.py
+++ college_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from django.contrib import admin
-from .views import chat_interaction_trends, generate_text
+from .views import  generate_text
 from .views import event_visualization
 from .views import activity_report
 from .views import internship_suggestion_view
